07c703813a4194b94f9cb8c69047e63f1ffeec17
Dave Dane Pacilan <davedanepacilan3p@gmail.com>
2016-12-25 16:02:21 +0800

    Add SoftDelete trait to post model

--- app/Post.php
+++ app/Post.php
@@ -3,12 +3,17 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Post extends Model
 {
+	use SoftDeletes;
+
 	protected $hidden = [
 		'user_id', 'updated_at', 'deleted_at',
-	]; 
+	];
+
+	protected $dates = ['deleted_at'];
 
     public function user()
     {
