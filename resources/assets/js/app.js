f4db2756d8f369359d1cd1b86e82edae76620ae4
Dave Dane Pacilan <davedanepacilan3p@gmail.com>
2017-01-28 23:33:21 +0800

    Register new components and update moved components

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -13,11 +13,22 @@
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
 
+// alert box components
 Vue.component('alert-danger', require('./components/alert-danger.vue'));
-Vue.component('panel-post', require('./components/panel-post.vue'));
-Vue.component('panel-poll', require('./components/panel-poll.vue'));
-Vue.component('bar-answer', require('./components/bar-answer.vue'));
-Vue.component('panel-suggestion', require('./components/panel-suggestion.vue'));
+
+// post components
+Vue.component('accordion-post', require('./components/post/accordion-post.vue'));
+Vue.component('panel-post', require('./components/post/panel-post.vue'));
+
+// poll components
+Vue.component('accordion-poll', require('./components/poll/accordion-poll.vue'));
+Vue.component('panel-poll', require('./components/poll/panel-poll.vue'));
+Vue.component('bar-answer', require('./components/poll/bar-answer.vue'));
+
+// suggestion components
+Vue.component('accordion-suggestion', require('./components/suggestion/accordion-suggestion.vue'));
+Vue.component('panel-suggestion', require('./components/suggestion/panel-suggestion.vue'));
+
 Vue.component('panel-search', require('./components/panel-search.vue'));
 
 const app = new Vue({
