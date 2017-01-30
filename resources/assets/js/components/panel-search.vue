3c2fd642a288185728372ee9bb85416116663ec0
Dave Dane Pacilan <davedanepacilan3p@gmail.com>
2017-01-30 18:12:02 +0800

    Add date to panel-search component

--- resources/assets/js/components/panel-search.vue
+++ resources/assets/js/components/panel-search.vue
@@ -1,6 +1,7 @@
 <template>
   <a :href="url" class="list-group-item list-item">
     <h4 class="list-group-item-heading" v-html="head"></h4>
+    <h4 v-if="actDate" class="list-group-item-heading"><small>{{actDate | formatDateTimeNormal}}</small></h4>
     <p class="list-group-item-text" v-html="body"></p>
   </a>
 </template>
@@ -20,10 +21,26 @@
         required: true
       },
 
+      actDate: {
+        type: String,
+        required: false,
+        default: null
+      },
+
       body: {
         type: String,
         required: true
       } 
+
+    },
+
+    filters: {
+
+      formatDateTimeNormal(date) {
+
+        return moment(date).format('MMM D, YYYY [at] h:mm a')
+
+      }
 
     }
 
