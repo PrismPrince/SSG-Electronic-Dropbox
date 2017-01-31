e4c4045846d8c827b4064c1fcbd0827ecd2db50d
Dave Dane Pacilan <davedanepacilan3p@gmail.com>
2017-02-01 01:00:19 +0800

    Filter search suggestions for student users

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Auth;
 use App\User;
 use App\Post;
 use App\Poll;
@@ -22,7 +23,10 @@ class SearchController extends Controller
     $users = User::searchName($request->key)->orderBy('fname')->orderBy('lname')->limit(3)->get();
     $posts = Post::searchTitle($request->key)->orderBy('created_at', 'desc')->limit(3)->get();
     $polls = Poll::searchTitle($request->key)->orderBy('created_at', 'desc')->limit(3)->get();
-    $suggestions = Suggestion::searchTitle($request->key)->orderBy('created_at', 'desc')->limit(3)->get();
+    if (Auth::guard('api')->user()->role == 'student')
+      $suggestions = Suggestion::searchTitle($request->key)->where('user_id', Auth::guard('api')->id())->orderBy('created_at', 'desc')->limit(3)->get();
+    else
+      $suggestions = Suggestion::searchTitle($request->key)->orderBy('created_at', 'desc')->limit(3)->get();
 
     return response()->json([
       'users' => $users,
@@ -48,25 +52,25 @@ class SearchController extends Controller
   {
     if ($request->month != null)
       $posts = Post::search($request->key)
-                    ->whereBetween('created_at', [
-                      Carbon::create($request->year, $request->month, 1, 12, 0, 0)->startOfMonth(),
-                      Carbon::create($request->year, $request->month, 1, 12, 0, 0)->endOfMonth()
-                    ])
-                    ->orderBy('created_at', 'desc')
-                    ->offset($request->skip)
-                    ->limit($request->take)
-                    ->get();
+        ->whereBetween('created_at', [
+          Carbon::create($request->year, $request->month, 1, 12, 0, 0)->startOfMonth(),
+          Carbon::create($request->year, $request->month, 1, 12, 0, 0)->endOfMonth()
+        ])
+        ->orderBy('created_at', 'desc')
+        ->offset($request->skip)
+        ->limit($request->take)
+        ->get();
 
     elseif ($request->year != null)
       $posts = Post::search($request->key)
-                    ->whereBetween('created_at', [
-                      Carbon::create($request->year, 1, 1, 12, 0, 0)->startOfYear(),
-                      Carbon::create($request->year, 1, 1, 12, 0, 0)->endOfYear()
-                    ])
-                    ->orderBy('created_at', 'desc')
-                    ->offset($request->skip)
-                    ->limit($request->take)
-                    ->get();
+        ->whereBetween('created_at', [
+          Carbon::create($request->year, 1, 1, 12, 0, 0)->startOfYear(),
+          Carbon::create($request->year, 1, 1, 12, 0, 0)->endOfYear()
+        ])
+        ->orderBy('created_at', 'desc')
+        ->offset($request->skip)
+        ->limit($request->take)
+        ->get();
 
     else
       $posts = Post::search($request->key)->orderBy('created_at', 'desc')->offset($request->skip)->limit($request->take)->get();
@@ -78,25 +82,25 @@ class SearchController extends Controller
   {
     if ($request->month != null)
       $polls = Poll::search($request->key)
-                    ->whereBetween('created_at', [
-                      Carbon::create($request->year, $request->month, 1, 12, 0, 0)->startOfMonth(),
-                      Carbon::create($request->year, $request->month, 1, 12, 0, 0)->endOfMonth()
-                    ])
-                    ->orderBy('created_at', 'desc')
-                    ->offset($request->skip)
-                    ->limit($request->take)
-                    ->get();
+        ->whereBetween('created_at', [
+          Carbon::create($request->year, $request->month, 1, 12, 0, 0)->startOfMonth(),
+          Carbon::create($request->year, $request->month, 1, 12, 0, 0)->endOfMonth()
+        ])
+        ->orderBy('created_at', 'desc')
+        ->offset($request->skip)
+        ->limit($request->take)
+        ->get();
 
     elseif ($request->year != null)
       $polls = Poll::search($request->key)
-                    ->whereBetween('created_at', [
-                      Carbon::create($request->year, 1, 1, 12, 0, 0)->startOfYear(),
-                      Carbon::create($request->year, 1, 1, 12, 0, 0)->endOfYear()
-                    ])
-                    ->orderBy('created_at', 'desc')
-                    ->offset($request->skip)
-                    ->limit($request->take)
-                    ->get();
+        ->whereBetween('created_at', [
+          Carbon::create($request->year, 1, 1, 12, 0, 0)->startOfYear(),
+          Carbon::create($request->year, 1, 1, 12, 0, 0)->endOfYear()
+        ])
+        ->orderBy('created_at', 'desc')
+        ->offset($request->skip)
+        ->limit($request->take)
+        ->get();
 
     else
       $polls = Poll::search($request->key)->orderBy('created_at', 'desc')->offset($request->skip)->limit($request->take)->get();
@@ -107,29 +111,56 @@ class SearchController extends Controller
   public function getSuggestions(Request $request)
   {
     if ($request->month != null)
-      $suggestions = Suggestion::search($request->key)
-                    ->whereBetween('created_at', [
-                      Carbon::create($request->year, $request->month, 1, 12, 0, 0)->startOfMonth(),
-                      Carbon::create($request->year, $request->month, 1, 12, 0, 0)->endOfMonth()
-                    ])
-                    ->orderBy('created_at', 'desc')
-                    ->offset($request->skip)
-                    ->limit($request->take)
-                    ->get();
+      if (Auth::guard('api')->user()->role == 'student')
+        $suggestions = Suggestion::search($request->key)
+          ->whereBetween('created_at', [
+            Carbon::create($request->year, $request->month, 1, 12, 0, 0)->startOfMonth(),
+            Carbon::create($request->year, $request->month, 1, 12, 0, 0)->endOfMonth()
+          ])
+          ->where('user_id', Auth::guard('api')->id())
+          ->orderBy('created_at', 'desc')
+          ->offset($request->skip)
+          ->limit($request->take)
+          ->get();
+      else
+        $suggestions = Suggestion::search($request->key)
+          ->whereBetween('created_at', [
+            Carbon::create($request->year, $request->month, 1, 12, 0, 0)->startOfMonth(),
+            Carbon::create($request->year, $request->month, 1, 12, 0, 0)->endOfMonth()
+          ])
+          ->orderBy('created_at', 'desc')
+          ->offset($request->skip)
+          ->limit($request->take)
+          ->get();
 
     elseif ($request->year != null)
-      $suggestions = Suggestion::search($request->key)
-                    ->whereBetween('created_at', [
-                      Carbon::create($request->year, 1, 1, 12, 0, 0)->startOfYear(),
-                      Carbon::create($request->year, 1, 1, 12, 0, 0)->endOfYear()
-                    ])
-                    ->orderBy('created_at', 'desc')
-                    ->offset($request->skip)
-                    ->limit($request->take)
-                    ->get();
+      if (Auth::guard('api')->user()->role == 'student')
+        $suggestions = Suggestion::search($request->key)
+          ->whereBetween('created_at', [
+            Carbon::create($request->year, 1, 1, 12, 0, 0)->startOfYear(),
+            Carbon::create($request->year, 1, 1, 12, 0, 0)->endOfYear()
+          ])
+          ->where('user_id', Auth::guard('api')->id())
+          ->orderBy('created_at', 'desc')
+          ->offset($request->skip)
+          ->limit($request->take)
+          ->get();
+      else
+        $suggestions = Suggestion::search($request->key)
+          ->whereBetween('created_at', [
+            Carbon::create($request->year, 1, 1, 12, 0, 0)->startOfYear(),
+            Carbon::create($request->year, 1, 1, 12, 0, 0)->endOfYear()
+          ])
+          ->orderBy('created_at', 'desc')
+          ->offset($request->skip)
+          ->limit($request->take)
+          ->get();
 
     else
-      $suggestions = Suggestion::search($request->key)->orderBy('created_at', 'desc')->offset($request->skip)->limit($request->take)->get();
+      if (Auth::guard('api')->user()->role == 'student')
+        $suggestions = Suggestion::search($request->key)->where('user_id', Auth::guard('api')->id())->orderBy('created_at', 'desc')->offset($request->skip)->limit($request->take)->get();
+      else
+        $suggestions = Suggestion::search($request->key)->orderBy('created_at', 'desc')->offset($request->skip)->limit($request->take)->get();
 
     return response()->json($suggestions);
   }
