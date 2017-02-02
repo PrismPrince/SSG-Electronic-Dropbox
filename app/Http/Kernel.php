baa86ea30407559e0855c6ffbc26fd10fedc513b
Dave Dane Pacilan <davedanepacilan3p@gmail.com>
2017-02-03 01:22:46 +0800

    Add admin middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -52,6 +52,7 @@ class Kernel extends HttpKernel
         'can' => \Illuminate\Auth\Middleware\Authorize::class,
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
+        'admin' => \App\Http\Middleware\RedirectIfNotAdmin::class,
         'student.suggestion' => \App\Http\Middleware\RedirectIfNotOwnStudentSuggestion::class,
     ];
 }
