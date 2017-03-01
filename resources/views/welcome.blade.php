93385f72d879b9860cb8d5dc26b7a19be035006c
Dave Dane Pacilan <davedanepacilan3p@gmail.com>
2017-03-01 16:16:53 +0800

    Modify target for external link to a new tab

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -133,14 +133,14 @@
           </div> {{-- .col-sm-3 --}}
           <div class="col-sm-9">
             <h1 class="text-center">About</h1>
-            <p><a href="https://www.facebook.com/CTUMCSSG">Tomorrow's Council</a> is the official Facebook page of the Supreme Student Government of Cebu Technological University - Main Campus.</p>
-            <p>The highest governing student body in <a href="https://http://www.ctu.edu.ph">Cebu Technological University - Main Campus</a>.</p>
+            <p><a target="_blank" href="https://www.facebook.com/CTUMCSSG">Tomorrow's Council</a> is the official Facebook page of the Supreme Student Government of Cebu Technological University - Main Campus.</p>
+            <p>The highest governing student body in <a target="_blank" href="http://www.ctu.edu.ph">Cebu Technological University - Main Campus</a>.</p>
           </div> {{-- .col-sm-9 --}}
         </div> {{-- .row --}}
 
         <div class="row">
           <div class="col-sm-3 hidden-xs">
-            <a href="http://www.ctu.edu.ph/transparency-seal/" style="display: block;padding: 0 20px;">
+            <a target="_blank" href="http://www.ctu.edu.ph/transparency-seal/" style="display: block;padding: 0 20px;">
               <img class="img-responsive" src="{{ asset('/images/school/pts.png') }}" alt="Philippine Transparency Seal">
             </a>
           </div> {{-- .col-sm-3 --}}
@@ -151,7 +151,7 @@
                 <h3 class="panel-title">Employee Portal</h3>
               </div> {{-- .panel-heading --}}
               <div class="panel-body">
-                <a href="http://www.ctu.edu.ph/transparency-seal/">
+                <a target="_blank" href="http://www.ctu.edu.ph/employee-portal/">
                   <img class="img-responsive" src="{{ asset('/images/school/employee-portal.png') }}" alt="Employee Portal">
                 </a>
               </div> {{-- .panel-body --}}
@@ -164,7 +164,7 @@
                 <h3 class="panel-title">Student Portal</h3>
               </div> {{-- .panel-heading --}}
               <div class="panel-body">
-                <a href="http://www.ctu.edu.ph/transparency-seal/">
+                <a target="_blank" href="http://www.ctu.edu.ph/studentwebapp/">
                   <img class="img-responsive" src="{{ asset('/images/school/student-portal.png') }}" alt="Student Portal">
                 </a>
               </div> {{-- .panel-body --}}
@@ -217,47 +217,47 @@
           <div class="col-sm-12">
             <ul class="list-inline">
               <li>
-                <a href="https://www.ctu.edu.ph">
+                <a target="_blank" href="http://www.ctu.edu.ph">
                   <img src="{{ asset('/images/campuses/main.jpg') }}" alt="Cebu Technological University - Main Campus">
                 </a>
               </li>
               <li>
-                <a href="https://www.argao.ctu.edu.ph">
+                <a target="_blank" href="http://www.argao.ctu.edu.ph">
                   <img src="{{ asset('/images/campuses/argao.jpg') }}" alt="Cebu Technological University - Argao Campus">
                 </a>
               </li>
               <li>
-                <a href="https://www.barili.ctu.edu.ph">
+                <a target="_blank" href="http://www.barili.ctu.edu.ph">
                   <img src="{{ asset('/images/campuses/barili.jpg') }}" alt="Cebu Technological University - Barili Campus">
                 </a>
               </li>
               <li>
-                <a href="https://www.carmen.ctu.edu.ph">
+                <a target="_blank" href="http://www.carmen.ctu.edu.ph">
                   <img src="{{ asset('/images/campuses/carmen.jpg') }}" alt="Cebu Technological University - Carmen Campus">
                 </a>
               </li>
               <li>
-                <a href="https://www.daanbantayan.ctu.edu.ph">
+                <a target="_blank" href="http://www.daanbantayan.ctu.edu.ph">
                   <img src="{{ asset('/images/campuses/daanbantayan.jpg') }}" alt="Cebu Technological University - Daanbantayan Campus">
                 </a>
               </li>
               <li>
-                <a href="https://www.danao.ctu.edu.ph">
+                <a target="_blank" href="http://www.danao.ctu.edu.ph">
                   <img src="{{ asset('/images/campuses/danao.jpg') }}" alt="Cebu Technological University - Danao Campus">
                 </a>
               </li>
               <li>
-                <a href="https://www.moalboal.ctu.edu.ph">
+                <a target="_blank" href="http://www.moalboal.ctu.edu.ph">
                   <img src="{{ asset('/images/campuses/moalboal.jpg') }}" alt="Cebu Technological University - Moalboal Campus">
                 </a>
               </li>
               <li>
-                <a href="https://www.sanfran.ctu.edu.ph">
+                <a target="_blank" href="http://www.sanfran.ctu.edu.ph">
                   <img src="{{ asset('/images/campuses/san-francisco.jpg') }}" alt="Cebu Technological University - San Francisco Campus">
                 </a>
               </li>
               <li>
-                <a href="https://www.tuburan.ctu.edu.ph">
+                <a target="_blank" href="http://www.tuburan.ctu.edu.ph">
                   <img src="{{ asset('/images/campuses/tuburan.jpg') }}" alt="Cebu Technological University - Tuburan Campus">
                 </a>
               </li>
@@ -286,28 +286,28 @@
               <a href="mailto:information@ctu.edu.ph">information@ctu.edu.ph</a>
             </address>
             <address>
-              <a href="https://www.ctu.edu.ph/departments/">Campus Directory</a>
+              <a target="_blank" href="http://www.ctu.edu.ph/departments/">Campus Directory</a>
             </address>
           </div> {{-- .col-sm-4 --}}
 
           <div class="col-sm-4">
             <h3>Quick links</h3>
             <ul class="list-unstyled">
-              <li><a href="https://www.ctu.edu.ph/list-of-vacant-positions/">List of Vacant Positions</a></li>
-              <li><a href="http://www.ctu.edu.ph/#">Online Job Application</a></li>
-              <li><a href="http://www.ctu.edu.ph/#">CTU Hymn</a></li>
-              <li><a href="http://www.ctu.edu.ph/#">CTU Main Alumni</a></li>
-              <li><a href="http://www.ctu.edu.ph/#">Graduate School Online</a></li>
-              <li><a href="http://www.ctu.edu.ph/feedback/">Feedback</a></li>
-              <li><a href="http://www.ctu.edu.ph/studentwebapp/">CTU WebApp</a></li>
-              <li><a href="http://www.ctu.edu.ph/online-public-access-catalog-opac/">Library OPAC</a></li>
-              <li><a href="http://www.ctu.edu.ph/library-proquest/">Library ProQuest</a></li>
+              <li><a target="_blank" href="http://www.ctu.edu.ph/list-of-vacant-positions/">List of Vacant Positions</a></li>
+              <li><a target="_blank" href="http://www.ctu.edu.ph/#">Online Job Application</a></li>
+              <li><a target="_blank" href="http://www.ctu.edu.ph/#">CTU Hymn</a></li>
+              <li><a target="_blank" href="http://www.ctu.edu.ph/#">CTU Main Alumni</a></li>
+              <li><a target="_blank" href="http://www.ctu.edu.ph/#">Graduate School Online</a></li>
+              <li><a target="_blank" href="http://www.ctu.edu.ph/feedback/">Feedback</a></li>
+              <li><a target="_blank" href="http://www.ctu.edu.ph/studentwebapp/">CTU WebApp</a></li>
+              <li><a target="_blank" href="http://www.ctu.edu.ph/online-public-access-catalog-opac/">Library OPAC</a></li>
+              <li><a target="_blank" href="http://www.ctu.edu.ph/library-proquest/">Library ProQuest</a></li>
               <li><hr></li>
-              <li><a href="http://www.ctu.edu.ph/#">Students</a></li>
-              <li><a href="http://www.ctu.edu.ph/#">Faculty</a></li>
-              <li><a href="http://www.ctu.edu.ph/#">Parents</a></li>
-              <li><a href="http://www.ctu.edu.ph/#">Visitors</a></li>
-              <li><a href="http://www.ctu.edu.ph/#">Staff</a></li>
+              <li><a target="_blank" href="http://www.ctu.edu.ph/#">Students</a></li>
+              <li><a target="_blank" href="http://www.ctu.edu.ph/#">Faculty</a></li>
+              <li><a target="_blank" href="http://www.ctu.edu.ph/#">Parents</a></li>
+              <li><a target="_blank" href="http://www.ctu.edu.ph/#">Visitors</a></li>
+              <li><a target="_blank" href="http://www.ctu.edu.ph/#">Staff</a></li>
             </ul> {{-- .list-unstyled --}}
           </div> {{-- .col-sm-4 --}}
 
@@ -315,7 +315,7 @@
             <h3>Government links</h3>
             <div class="row">
               <div class="col-xs-12">
-                <a href="http://www.gov.ph/" title="OFFICIAL GAZETTE OF THE REPUBLIC OF THE PHILIPPINES">
+                <a target="_blank" href="http://www.gov.ph/" title="OFFICIAL GAZETTE OF THE REPUBLIC OF THE PHILIPPINES">
                   <img src="{{ asset('/images/government/official-gazette.png') }}" alt="OFFICIAL GAZETTE OF THE REPUBLIC OF THE PHILIPPINES">
                 </a>
               </div> {{-- .col-xs-12 --}}
@@ -323,17 +323,17 @@
 
             <div class="row">
               <div class="col-xs-4">
-                <a href="http://www.coa.gov.ph/" title="COMMISSION ON AUDIT(COA)">
+                <a target="_blank" href="http://www.coa.gov.ph/" title="COMMISSION ON AUDIT(COA)">
                   <img src="{{ asset('/images/government/coa.png') }}" alt="COMMISSION ON AUDIT(COA)">
                 </a>
               </div> {{-- .col-xs-4 --}}
               <div class="col-xs-4">
-                <a href="http://www.dost.gov.ph/" title="Department of Science and Technology (DOST)">
+                <a target="_blank" href="http://www.dost.gov.ph/" title="Department of Science and Technology (DOST)">
                   <img src="{{ asset('images/government/dost.png') }}" alt="Department of Science and Technology (DOST)">
                 </a>
               </div> {{-- .col-xs-4 --}}
               <div class="col-xs-4">
-                <a href="http://www.ched.gov.ph/" title="COMMISSION ON HIGHER EDUCATION (CHED)">
+                <a target="_blank" href="http://www.ched.gov.ph/" title="COMMISSION ON HIGHER EDUCATION (CHED)">
                   <img src="{{ asset('/images/government/ched.png') }}" alt="COMMISSION ON HIGHER EDUCATION (CHED)">
                 </a>
               </div> {{-- .col-xs-4 --}}
@@ -341,17 +341,17 @@
 
             <div class="row">
               <div class="col-xs-4">
-                <a href="http://www.philgeps.gov.ph/" title="PHILIPPINE GOVERNMENT ELECTRONIC PROCUREMENT SYSTEM (PHILGEPS)">
+                <a target="_blank" href="http://www.philgeps.gov.ph/" title="PHILIPPINE GOVERNMENT ELECTRONIC PROCUREMENT SYSTEM (PHILGEPS)">
                   <img src="{{ asset('/images/government/philgeps.png') }}" alt="PHILIPPINE GOVERNMENT ELECTRONIC PROCUREMENT SYSTEM (PHILGEPS)">
                 </a>
               </div> {{-- .col-xs-4 --}}
               <div class="col-xs-4">
-                <a href="http://www.doe.gov.ph/" title="DEPARTMENT OF ENERGY (DOE)">
+                <a target="_blank" href="http://www.doe.gov.ph/" title="DEPARTMENT OF ENERGY (DOE)">
                   <img src="{{ asset('/images/government/doe.png') }}" alt="DEPARTMENT OF ENERGY (DOE)">
                 </a>
               </div> {{-- .col-xs-4 --}}
               <div class="col-xs-4">
-                <a href="http://www.doh.gov.ph/" title="DEPARTMENT OF HEALTH (DOH)">
+                <a target="_blank" href="http://www.doh.gov.ph/" title="DEPARTMENT OF HEALTH (DOH)">
                   <img src="{{ asset('/images/government/doh.png') }}" alt="DEPARTMENT OF HEALTH (DOH)">
                 </a>
               </div> {{-- .col-xs-4 --}}
@@ -359,17 +359,17 @@
 
             <div class="row">
               <div class="col-xs-4">
-                <a href="http://itsmorefuninthephilippines.com/" title="DEPARTMENT OF TOURISM (DOT)">
+                <a target="_blank" href="http://itsmorefuninthephilippines.com/" title="DEPARTMENT OF TOURISM (DOT)">
                   <img src="{{ asset('/images/government/dot.png') }}" alt="DEPARTMENT OF TOURISM (DOT)">
                 </a>
               </div> {{-- .col-xs-4 --}}
               <div class="col-xs-4">
-                <a href="http://www.dole.gov.ph/" title="DEPARTMENT OF LABOR AND EMPLOYMENT (DOLE)">
+                <a target="_blank" href="http://www.dole.gov.ph/" title="DEPARTMENT OF LABOR AND EMPLOYMENT (DOLE)">
                   <img src="{{ asset('/images/government/dole.png') }}" alt="DEPARTMENT OF LABOR AND EMPLOYMENT (DOLE)">
                 </a>
               </div> {{-- .col-xs-4 --}}
               <div class="col-xs-4">
-                <a href="http://www.dof.gov.ph/" title="DEPARTMENT OF FINANCE (DOF)">
+                <a target="_blank" href="http://www.dof.gov.ph/" title="DEPARTMENT OF FINANCE (DOF)">
                   <img src="{{ asset('/images/government/dof.png') }}" alt="DEPARTMENT OF FINANCE (DOF)">
                 </a>
               </div> {{-- .col-xs-4 --}}
@@ -381,7 +381,7 @@
 
     <footer class="text-center">
       <div class="container">
-        <a href="http://www.ctu.edu.ph/terms-of-use/">Terms of Use</a> | <a href="http://www.ctu.edu.ph/privacy-statement/">Privacy Statement</a>
+        <a target="_blank" href="http://www.ctu.edu.ph/terms-of-use/">Terms of Use</a> | <a target="_blank" href="http://www.ctu.edu.ph/privacy-statement/">Privacy Statement</a>
         <h3>Cebu Technological University - Main campus <span class="glyphicon glyphicon-copyright-mark"></span> 2017</h3>
       </div> {{-- .container --}}
     </footer>
