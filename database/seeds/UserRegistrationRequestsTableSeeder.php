8ed243dc591cc80889845bf7fe69f8a20890ffd6
Dave Dane Pacilan <davedanepacilan3p@gmail.com>
2017-03-07 04:52:08 +0800

    Add more users

--- database/seeds/UserRegistrationRequestsTableSeeder.php
+++ database/seeds/UserRegistrationRequestsTableSeeder.php
@@ -10,29 +10,148 @@ class UserRegistrationRequestsTableSeeder extends Seeder
   public function run()
   {
     DB::table('user_registration_requests')->insert([
-      'id'         => 1000000, // init
+      'id'         => 1000000,
       'code'       => strtoupper(substr(md5(time()), 0, 5) . '-' . substr(md5(1000000), 0, 5) . '-' . str_random(5)),
       'created_at' => '2017-1-1',
       'updated_at' => '2017-1-1',
     ]);
 
     DB::table('user_registration_requests')->insert([
-      'id'         => 1000001, // init
+      'id'         => 1000001,
       'code'       => strtoupper(substr(md5(time()), 0, 5) . '-' . substr(md5(1000001), 0, 5) . '-' . str_random(5)),
       'created_at' => '2017-1-1',
       'updated_at' => '2017-1-1',
     ]);
 
     DB::table('user_registration_requests')->insert([
-      'id'         => 1000002, // init
+      'id'         => 1000002,
       'code'       => strtoupper(substr(md5(time()), 0, 5) . '-' . substr(md5(1000002), 0, 5) . '-' . str_random(5)),
       'created_at' => '2017-1-1',
       'updated_at' => '2017-1-1',
     ]);
 
     DB::table('user_registration_requests')->insert([
-      'id'         => 1000003, // init
+      'id'         => 1000003,
       'code'       => strtoupper(substr(md5(time()), 0, 5) . '-' . substr(md5(1000003), 0, 5) . '-' . str_random(5)),
+      'created_at' => '2017-1-1',
+      'updated_at' => '2017-1-1',
+    ]);
+
+    DB::table('user_registration_requests')->insert([
+      'id'         => 1000004,
+      'code'       => strtoupper(substr(md5(time()), 0, 5) . '-' . substr(md5(1000004), 0, 5) . '-' . str_random(5)),
+      'created_at' => '2017-1-1',
+      'updated_at' => '2017-1-1',
+    ]);
+
+    DB::table('user_registration_requests')->insert([
+      'id'         => 1000005,
+      'code'       => strtoupper(substr(md5(time()), 0, 5) . '-' . substr(md5(1000005), 0, 5) . '-' . str_random(5)),
+      'created_at' => '2017-1-1',
+      'updated_at' => '2017-1-1',
+    ]);
+
+    DB::table('user_registration_requests')->insert([
+      'id'         => 1000006,
+      'code'       => strtoupper(substr(md5(time()), 0, 5) . '-' . substr(md5(1000006), 0, 5) . '-' . str_random(5)),
+      'created_at' => '2017-1-1',
+      'updated_at' => '2017-1-1',
+    ]);
+
+    DB::table('user_registration_requests')->insert([
+      'id'         => 1000007,
+      'code'       => strtoupper(substr(md5(time()), 0, 5) . '-' . substr(md5(1000007), 0, 5) . '-' . str_random(5)),
+      'created_at' => '2017-1-1',
+      'updated_at' => '2017-1-1',
+    ]);
+
+    DB::table('user_registration_requests')->insert([
+      'id'         => 1000008,
+      'code'       => strtoupper(substr(md5(time()), 0, 5) . '-' . substr(md5(1000008), 0, 5) . '-' . str_random(5)),
+      'created_at' => '2017-1-1',
+      'updated_at' => '2017-1-1',
+    ]);
+
+    DB::table('user_registration_requests')->insert([
+      'id'         => 1000009,
+      'code'       => strtoupper(substr(md5(time()), 0, 5) . '-' . substr(md5(1000009), 0, 5) . '-' . str_random(5)),
+      'created_at' => '2017-1-1',
+      'updated_at' => '2017-1-1',
+    ]);
+
+    DB::table('user_registration_requests')->insert([
+      'id'         => 1000010,
+      'code'       => strtoupper(substr(md5(time()), 0, 5) . '-' . substr(md5(1000010), 0, 5) . '-' . str_random(5)),
+      'created_at' => '2017-1-1',
+      'updated_at' => '2017-1-1',
+    ]);
+
+    DB::table('user_registration_requests')->insert([
+      'id'         => 1000011,
+      'code'       => strtoupper(substr(md5(time()), 0, 5) . '-' . substr(md5(1000011), 0, 5) . '-' . str_random(5)),
+      'created_at' => '2017-1-1',
+      'updated_at' => '2017-1-1',
+    ]);
+
+    DB::table('user_registration_requests')->insert([
+      'id'         => 1000012,
+      'code'       => strtoupper(substr(md5(time()), 0, 5) . '-' . substr(md5(1000012), 0, 5) . '-' . str_random(5)),
+      'created_at' => '2017-1-1',
+      'updated_at' => '2017-1-1',
+    ]);
+
+    DB::table('user_registration_requests')->insert([
+      'id'         => 1000013,
+      'code'       => strtoupper(substr(md5(time()), 0, 5) . '-' . substr(md5(1000013), 0, 5) . '-' . str_random(5)),
+      'created_at' => '2017-1-1',
+      'updated_at' => '2017-1-1',
+    ]);
+
+    DB::table('user_registration_requests')->insert([
+      'id'         => 1000014,
+      'code'       => strtoupper(substr(md5(time()), 0, 5) . '-' . substr(md5(1000014), 0, 5) . '-' . str_random(5)),
+      'created_at' => '2017-1-1',
+      'updated_at' => '2017-1-1',
+    ]);
+
+    DB::table('user_registration_requests')->insert([
+      'id'         => 1000015,
+      'code'       => strtoupper(substr(md5(time()), 0, 5) . '-' . substr(md5(1000015), 0, 5) . '-' . str_random(5)),
+      'created_at' => '2017-1-1',
+      'updated_at' => '2017-1-1',
+    ]);
+
+    DB::table('user_registration_requests')->insert([
+      'id'         => 1000016,
+      'code'       => strtoupper(substr(md5(time()), 0, 5) . '-' . substr(md5(1000016), 0, 5) . '-' . str_random(5)),
+      'created_at' => '2017-1-1',
+      'updated_at' => '2017-1-1',
+    ]);
+
+    DB::table('user_registration_requests')->insert([
+      'id'         => 1000017,
+      'code'       => strtoupper(substr(md5(time()), 0, 5) . '-' . substr(md5(1000017), 0, 5) . '-' . str_random(5)),
+      'created_at' => '2017-1-1',
+      'updated_at' => '2017-1-1',
+    ]);
+
+    DB::table('user_registration_requests')->insert([
+      'id'         => 1000018,
+      'code'       => strtoupper(substr(md5(time()), 0, 5) . '-' . substr(md5(1000018), 0, 5) . '-' . str_random(5)),
+      'created_at' => '2017-1-1',
+      'updated_at' => '2017-1-1',
+    ]);
+
+    DB::table('user_registration_requests')->insert([
+      'id'         => 1000019,
+      'code'       => strtoupper(substr(md5(time()), 0, 5) . '-' . substr(md5(1000019), 0, 5) . '-' . str_random(5)),
+      'created_at' => '2017-1-1',
+      'updated_at' => '2017-1-1',
+    ]);
+
+    DB::table('user_registration_requests')->insert([
+      'id'         => 1000020,
+      'code'       => strtoupper(substr(md5(time()), 0, 5) . '-' . substr(md5(1000020), 0, 5) . '-' . str_random(5)),
       'created_at' => '2017-1-1',
       'updated_at' => '2017-1-1',
     ]);
--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -56,6 +56,193 @@ class UsersTableSeeder extends Seeder
       'updated_at' => '2017-1-1',
     ]);
 
+    DB::table('users')->insert([
+      'id'         => 1000004,
+      'fname'      => 'John',
+      'lname'      => 'Mayer',
+      'email'      => 'mayer@gmail.com',
+      'password'   => bcrypt('123456'),
+      'api_token'  => str_random(60),
+      'created_at' => '2017-1-1',
+      'updated_at' => '2017-1-1',
+    ]);
+
+    DB::table('users')->insert([
+      'id'         => 1000005,
+      'fname'      => 'Lady',
+      'lname'      => 'Gaga',
+      'email'      => 'lady_gaga@gmail.com',
+      'password'   => bcrypt('123456'),
+      'api_token'  => str_random(60),
+      'created_at' => '2017-1-1',
+      'updated_at' => '2017-1-1',
+    ]);
+
+    DB::table('users')->insert([
+      'id'         => 1000006,
+      'fname'      => 'Sarah',
+      'lname'      => 'Geronimo',
+      'email'      => 'sarah123@gmail.com',
+      'password'   => bcrypt('123456'),
+      'api_token'  => str_random(60),
+      'created_at' => '2017-1-1',
+      'updated_at' => '2017-1-1',
+    ]);
+
+    DB::table('users')->insert([
+      'id'         => 1000007,
+      'fname'      => 'Eric',
+      'lname'      => 'Santos',
+      'email'      => 'santoseric@gmail.com',
+      'password'   => bcrypt('123456'),
+      'api_token'  => str_random(60),
+      'created_at' => '2017-1-1',
+      'updated_at' => '2017-1-1',
+    ]);
+
+    DB::table('users')->insert([
+      'id'         => 1000008,
+      'fname'      => 'Piolo',
+      'lname'      => 'Pascual',
+      'email'      => 'papaPee@gmail.com',
+      'password'   => bcrypt('123456'),
+      'api_token'  => str_random(60),
+      'created_at' => '2017-1-1',
+      'updated_at' => '2017-1-1',
+    ]);
+
+    DB::table('users')->insert([
+      'id'         => 1000009,
+      'fname'      => 'Quen',
+      'lname'      => 'Gil',
+      'email'      => 'enrique_gil@gmail.com',
+      'password'   => bcrypt('123456'),
+      'api_token'  => str_random(60),
+      'created_at' => '2017-1-1',
+      'updated_at' => '2017-1-1',
+    ]);
+
+    DB::table('users')->insert([
+      'id'         => 1000010,
+      'fname'      => 'Vice',
+      'lname'      => 'Ganda',
+      'email'      => 'josemarieviceral@gmail.com',
+      'password'   => bcrypt('123456'),
+      'api_token'  => str_random(60),
+      'created_at' => '2017-1-1',
+      'updated_at' => '2017-1-1',
+    ]);
+
+    DB::table('users')->insert([
+      'id'         => 1000011,
+      'fname'      => 'Justin',
+      'lname'      => 'Bebier',
+      'email'      => 'justin@gmail.com',
+      'password'   => bcrypt('123456'),
+      'api_token'  => str_random(60),
+      'created_at' => '2017-1-1',
+      'updated_at' => '2017-1-1',
+    ]);
+
+    DB::table('users')->insert([
+      'id'         => 1000012,
+      'fname'      => 'Jake',
+      'lname'      => 'Sali',
+      'email'      => 'sali_jake@gmail.com',
+      'password'   => bcrypt('123456'),
+      'api_token'  => str_random(60),
+      'created_at' => '2017-1-1',
+      'updated_at' => '2017-1-1',
+    ]);
+
+    DB::table('users')->insert([
+      'id'         => 1000013,
+      'fname'      => 'Princess',
+      'lname'      => 'Aurora',
+      'email'      => 'sleeping_beauty@gmail.com',
+      'password'   => bcrypt('123456'),
+      'api_token'  => str_random(60),
+      'created_at' => '2017-1-1',
+      'updated_at' => '2017-1-1',
+    ]);
+
+    DB::table('users')->insert([
+      'id'         => 1000014,
+      'fname'      => 'Snow',
+      'lname'      => 'White',
+      'email'      => 'snow7@gmail.com',
+      'password'   => bcrypt('123456'),
+      'api_token'  => str_random(60),
+      'created_at' => '2017-1-1',
+      'updated_at' => '2017-1-1',
+    ]);
+
+    DB::table('users')->insert([
+      'id'         => 1000015,
+      'fname'      => 'Revica',
+      'lname'      => 'Black',
+      'email'      => 'black@gmail.com',
+      'password'   => bcrypt('123456'),
+      'api_token'  => str_random(60),
+      'created_at' => '2017-1-1',
+      'updated_at' => '2017-1-1',
+    ]);
+
+    DB::table('users')->insert([
+      'id'         => 1000016,
+      'fname'      => 'Charlie',
+      'lname'      => 'Green',
+      'email'      => 'green@gmail.com',
+      'password'   => bcrypt('123456'),
+      'api_token'  => str_random(60),
+      'created_at' => '2017-1-1',
+      'updated_at' => '2017-1-1',
+    ]);
+
+    DB::table('users')->insert([
+      'id'         => 1000017,
+      'fname'      => 'JC',
+      'lname'      => 'May',
+      'email'      => 'mayJC@gmail.com',
+      'password'   => bcrypt('123456'),
+      'api_token'  => str_random(60),
+      'created_at' => '2017-1-1',
+      'updated_at' => '2017-1-1',
+    ]);
+
+    DB::table('users')->insert([
+      'id'         => 1000018,
+      'fname'      => 'Fred',
+      'lname'      => 'Wood',
+      'email'      => 'woody@gmail.com',
+      'password'   => bcrypt('123456'),
+      'api_token'  => str_random(60),
+      'created_at' => '2017-1-1',
+      'updated_at' => '2017-1-1',
+    ]);
+
+    DB::table('users')->insert([
+      'id'         => 1000019,
+      'fname'      => 'Mark',
+      'lname'      => 'Cordova',
+      'email'      => 'cordova@gmail.com',
+      'password'   => bcrypt('123456'),
+      'api_token'  => str_random(60),
+      'created_at' => '2017-1-1',
+      'updated_at' => '2017-1-1',
+    ]);
+
+    DB::table('users')->insert([
+      'id'         => 1000020,
+      'fname'      => 'Jane',
+      'lname'      => 'Titan',
+      'email'      => 'titan@gmail.com',
+      'password'   => bcrypt('123456'),
+      'api_token'  => str_random(60),
+      'created_at' => '2017-1-1',
+      'updated_at' => '2017-1-1',
+    ]);
+
     // Generate fake data
 
     // $faker = Factory::create();
