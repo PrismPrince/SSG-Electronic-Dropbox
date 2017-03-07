99c14b73b7b160e9a3dfd7139c5813d7a9dbb10f
Dave Dane Pacilan <davedanepacilan3p@gmail.com>
2017-03-07 05:55:37 +0000

    Apply fixes from StyleCI

--- app/Http/Controllers/UserRegistrationRequestController.php
+++ app/Http/Controllers/UserRegistrationRequestController.php
@@ -17,7 +17,6 @@ class UserRegistrationRequestController extends Controller
   {
     if (isset($request->key))
       return response()->json(UserRegistrationRequest::where('id', 'LIKE', '%' . $request->key . '%')->offset($request->skip)->limit($request->take)->orderBy('created_at', 'desc')->get());
-    else
       return response()->json(UserRegistrationRequest::offset($request->skip)->limit($request->take)->orderBy('created_at', 'desc')->get());
   }
 
