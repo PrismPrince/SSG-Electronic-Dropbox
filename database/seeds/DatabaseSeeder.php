11e5f6c520c48295859e76dd3a35e75028853e34
Dave Dane Pacilan <davedanepacilan3p@gmail.com>
2017-03-23 01:36:06 +0800

    Initialize seeder to one administrator user

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -9,13 +9,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(UserRegistrationRequestsTableSeeder::class);
         $this->call(UsersTableSeeder::class);
-        $this->call(PostsTableSeeder::class);
-        $this->call(PhotosTableSeeder::class);
-        $this->call(PollsTableSeeder::class);
-        $this->call(AnswersTableSeeder::class);
-        $this->call(SuggestionsTableSeeder::class);
-        $this->call(CommentsTableSeeder::class);
     }
 }
