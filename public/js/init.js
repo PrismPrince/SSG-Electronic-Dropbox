fe1a3dc22a15ea27b0e14face7dd0a973744acb9
Dave Dane Pacilan <davedanepacilan3p@gmail.com>
2017-03-27 21:54:11 +0800

    Update .gitignore files

---
+++ public/js/init.js
@@ -0,0 +1,27 @@
+/*!!
+ * SSG Electronic Dropbox (https://www.github.com/PrismPrince/SSG-Electronic-Dropbox)
+ * Copyright 2017 Dave Dane Pacilan
+ * Licensed under MIT (https://github.com/PrismPrince/SSG-Electronic-Dropbox/blob/master/LICENSE)
+ */
+
+function initMap() {
+  var uluru = {lat: 10.295861, lng: 123.906303};
+
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 17,
+    center: uluru
+  });
+
+  var marker = new google.maps.Marker({
+    position: uluru,
+    map: map
+  });
+}
+
+(function(d, s, id) {
+	var js, fjs = d.getElementsByTagName(s)[0];
+	if (d.getElementById(id)) return;
+	js = d.createElement(s); js.id = id;
+	js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.8";
+	fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
